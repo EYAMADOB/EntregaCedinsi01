d21f7efd0905e7c21b126e6ae022abeb31ef97b2
EYAMADOB <aedwinyair@gmail.com>
2024-01-25 21:04:12 -0500

    Inicio Ejerccicios JS

---
+++ EJERCICIOS/modulo.js
@@ -0,0 +1,64 @@
+"use strict"
+//Primer Ejercicio
+/*let litros =0;
+let aconvertir=0;
+const decilitro =10;
+const centilitro =100;
+const galones =0.264;
+const onzas = 33.814;
+let resultado =0;
+litros= prompt("digita cantidad de litros a convertir");
+aconvertir=prompt("digita para convertir decilitros 1 - centilitros 2 - galones 3 - onzas 4 "," ");
+if (parseInt(aconvertir)===1)
+    {
+        resultado=parseInt(litros)/decilitro;
+        console.log(resultado)
+    }
+    if (parseInt(aconvertir)===2)
+    {
+        resultado=parseInt(litros)/centilitro;
+        console.log(resultado)
+    }
+    if (parseInt(aconvertir)===3)
+    {
+        resultado=parseInt(litros)/galones;
+        console.log(resultado)
+    }
+    if (parseInt(aconvertir)===4)
+    {
+        resultado=parseInt(litros)/galones;
+        console.log(resultado);
+    }
+*/
+//Segundo Ejercicio en construccion
+let numero1 =0;
+let numero2 =0;
+let aconvertir=0;
+const decilitro =10;
+const centilitro =100;
+const galones =0.264;
+const onzas = 33.814;
+let resultado =0;
+numero1= prompt("digita prmier numero");
+numero2= prompt("digita segundo numero");
+toperacion=prompt("digita para; sumar 1 - restar 2 - dividir 3 - multiplicar 4 "," ");
+if (parseInt(aconvertir)===1)
+    {
+        resultado=parseInt(litros)/decilitro;
+        console.log(resultado)
+    }
+    if (parseInt(aconvertir)===2)
+    {
+        resultado=parseInt(litros)/centilitro;
+        console.log(resultado)
+    }
+    if (parseInt(aconvertir)===3)
+    {
+        resultado=parseInt(litros)/galones;
+        console.log(resultado)
+    }
+    if (parseInt(aconvertir)===4)
+    {
+        resultado=parseInt(litros)/galones;
+        console.log(resultado);
+    }
